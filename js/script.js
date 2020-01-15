058fe12b78ed0d979381c5cb8fe2724b5acc9193
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-15 15:10:21 +0100

    tentativo function controllo numeri

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Il computer deve generare 16 numeri casuali da 1 a 100.
-// In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
+//
 // La partita termina quando il giocatore inserisce un numero “vietato”, ovvero presente nella lista di numeri random, o raggiunge il numero massimo possibile di tentativi consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
@@ -14,20 +14,40 @@ while (elencoNumeriPc.length < 16){
   }
 }
 
+console.log(elencoNumeriPc);
 
-var risultato = 0;
+// In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
 
-while (tentativiUtente.length < tentativiMassimi && controlloRisultato(elencoNumeriPc, numeroUtente) == false){
-  var numeroUtente = parseInt(prompt('inserisci un numero da 1 a 100'));
-  if (){
-
+function controlloNumeri (arrayDaControllare, numero) {
+  var risulato = false;
+  if (numero == arrayDaControllare){
+    risultato = true;
+    return risulato;
   }
+  return risultato;
+}
+
+
+
+
+
+
+
+
+
+
+
+// while (tentativiUtente.length < tentativiMassimi && controlloRisultato(elencoNumeriPc, numeroUtente) == false){
+//   var numeroUtente = parseInt(prompt('inserisci un numero da 1 a 100'));
+//   if (){
+//
+//   }
   // controllo non sia una bomba
   // controllo che il numero non sia già stato dato dall'utente
 
 
-
-}
+//
+// }
 
 // var numeroUtente = parseInt(prompt('inserisci un numero da 1 a 100'));
 //
