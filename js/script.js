6084256c7d2ded1d83502b265b5cc910be4599fd
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-15 18:11:20 +0100

    tolti commenti superflui

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 
 var elencoNumeriPc = [];
 var tentativiUtente = [];
-var tentativiMassimi = 7;
+var tentativiMassimi = 84;
 
 while (elencoNumeriPc.length < 16){
   var numeroRandom = numeriPc (1, 100);
@@ -35,46 +35,6 @@ while (i< tentativiMassimi && (controlloNumeri(elencoNumeriPc, numeroUtente) == 
  i++;
 }
 
-
-
-
-
-
-
-
-
-
-// while (tentativiUtente.length < tentativiMassimi && controlloRisultato(elencoNumeriPc, numeroUtente) == false){
-//   var numeroUtente = parseInt(prompt('inserisci un numero da 1 a 100'));
-//   if (){
-//
-//   }
-  // controllo non sia una bomba
-  // controllo che il numero non sia già stato dato dall'utente
-
-
-//
-// }
-
-// var numeroUtente = parseInt(prompt('inserisci un numero da 1 a 100'));
-//
-// var i =0;
-// while (i<84){
-//   if (numeroUtente == elencoNumeriPc){
-//     alert('spiacente, hai perso');
-//   }
-//   else if (numeroUtente != elencoNumeriPc) {
-//     numeroUtente = parseInt(prompt('inserisci un altro numero da 1 a 100'));
-//   }
-//   else {
-//     alert('alla gande! Hai vinto!');
-//   }
-//   i++;
-//   console.log(numeroUtente);
-// }
-//
-// var risultato = [];
-// risultato.push(numeroUtente);
 
 
 // funzioni
