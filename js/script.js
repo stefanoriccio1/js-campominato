831120ffbd6569836bcce1dee47fee8ea0141fa1
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-15 12:20:05 +0100

    up

--- js/script.js
+++ js/script.js
@@ -4,42 +4,66 @@
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 var elencoNumeriPc = [];
+var tentativiUtente = [];
+var tentativiMassimi = 84;
 
-for (i=0; i<16; i++){
-  numeroRandom = numeriPc (1, 100);
-  elencoNumeriPc.push(numeroRandom);
-}
-console.log(elencoNumeriPc);
-
-var numeroUtente = parseInt(prompt('inserisci un numero da 1 a 100'));
-
-var i =0;
-while (i<84){
-  if (numeroUtente == elencoNumeriPc){
-    alert('spiacente, hai perso');
+while (elencoNumeriPc.length < 16){
+  var numeroRandom = numeriPc (1, 100);
+  if (elencoNumeriPc.includes(numeroRandom) == false){
+    elencoNumeriPc.push(numeroRandom);
   }
-  else if (numeroUtente != elencoNumeriPc) {
-    numeroUtente = parseInt(prompt('inserisci un altro numero da 1 a 100'));
-  }
-  else {
-    alert('alla gande! Hai vinto!');
-  }
-  i++;
-  console.log(numeroUtente);
 }
 
-var risultato = [];
-risultato.push(numeroUtente);
+
+var risultato = 0;
+
+while (tentativiUtente.length < tentativiMassimi && controlloRisultato(elencoNumeriPc, numeroUtente) == false){
+  var numeroUtente = parseInt(prompt('inserisci un numero da 1 a 100'));
+  if (){
+
+  }
+  // controllo non sia una bomba
+  // controllo che il numero non sia già stato dato dall'utente
+
+
+
+}
+
+// var numeroUtente = parseInt(prompt('inserisci un numero da 1 a 100'));
+//
+// var i =0;
+// while (i<84){
+//   if (numeroUtente == elencoNumeriPc){
+//     alert('spiacente, hai perso');
+//   }
+//   else if (numeroUtente != elencoNumeriPc) {
+//     numeroUtente = parseInt(prompt('inserisci un altro numero da 1 a 100'));
+//   }
+//   else {
+//     alert('alla gande! Hai vinto!');
+//   }
+//   i++;
+//   console.log(numeroUtente);
+// }
+//
+// var risultato = [];
+// risultato.push(numeroUtente);
 
 
 // funzioni
 function numeriPc (min, max){
   min = Math.ceil(min);
   max = Math.floor (max);
-  var random = Math.floor(Math.random() * (max - min + 1));
+  var random = Math.floor(Math.random() * (max - min + 1)) + min;
   return random;
 }
 
-function numeriUtente (){
-
+// funzione di controllo risultato
+function controlloRisultato(arrayDaControllare, numeroDaControllare){
+  for (var i = 0; i < arrayDaControllare.length; i++) {
+    if (numeroDaControllare == arrayDaControllare[i]){
+      return true;
+    }
+  }
+  return false;
 }
